38c557513a309d476d7bfcec7e048ff853ebcf3f
Gusarov <alexandergusarovv@gmail.com>
2024-02-24 14:39:33 +0300

    chore: new deps, upd env and cfg

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
 import path from 'path';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import pluginChecker from 'vite-plugin-checker';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,19 +12,26 @@ export default defineConfig({
   build: {
     outDir: 'build',
     chunkSizeWarningLimit: 2000,
- },
+  },
+  server: {
+    port: 3000,
+    open: true,
+    //   watch: { // WSL
+    // 	usePolling: true
+    //   }
+  },
   resolve: {
     alias: {
-       '@ui': `${path.resolve(__dirname, './src/shared/ui')}`,
-       '@ui/*': `${path.resolve(__dirname, './src/shared/ui/*')}`,
-       '@shared': `${path.resolve(__dirname, './src/shared/')}`,
-       '@shared/*': `${path.resolve(__dirname, './src/shared/*')}`,
-       '@app': `${path.resolve(__dirname, './src/app/')}`,
-       '@app/*': `${path.resolve(__dirname, './src/app/*')}`,
-       '@pages': `${path.resolve(__dirname, './src/pages/')}`,
-       '@pages/*': `${path.resolve(__dirname, './src/pages/*')}`,
-       '@layout': `${path.resolve(__dirname, './src/layout/')}`,
-       '@layout/*': `${path.resolve(__dirname, './src/layout/*')}`,
+      '@ui': `${path.resolve(__dirname, './src/shared/ui')}`,
+      '@ui/*': `${path.resolve(__dirname, './src/shared/ui/*')}`,
+      '@shared': `${path.resolve(__dirname, './src/shared/')}`,
+      '@shared/*': `${path.resolve(__dirname, './src/shared/*')}`,
+      '@app': `${path.resolve(__dirname, './src/app/')}`,
+      '@app/*': `${path.resolve(__dirname, './src/app/*')}`,
+      '@pages': `${path.resolve(__dirname, './src/pages/')}`,
+      '@pages/*': `${path.resolve(__dirname, './src/pages/*')}`,
+      '@layout': `${path.resolve(__dirname, './src/layout/')}`,
+      '@layout/*': `${path.resolve(__dirname, './src/layout/*')}`,
     },
- },
+  },
 });
