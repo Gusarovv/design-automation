4beeff3b725dc41895637b751fff79b6d0a7c032
Gusarov <alexandergusarovv@gmail.com>
2024-02-26 07:16:36 +0300

    chore: dm @material-tailwind/react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
-import { ThemeProvider } from '@material-tailwind/react';
-
 import App from './app/App.tsx';
 import './app/style/global.css';
 import { store } from './shared/store/store.ts';
@@ -11,9 +9,7 @@ import { ErrorBoundary } from './shared/util/ErrorBoundary';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
     <Provider store={store}>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <App />
     </Provider>
   </ErrorBoundary>,
 );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const withMT = require("@material-tailwind/react/utils/withMT");
-
-module.exports = withMT({
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -11,4 +9,4 @@ module.exports = withMT({
     extend: {},
   },
   plugins: [],
-});
+};
