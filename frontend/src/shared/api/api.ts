f8bbcad5ec94fb7ce7c16e417fd4d0834b83419a
Gusarov <alexandergusarovv@gmail.com>
2024-02-26 10:59:10 +0300

    feat: use json-bigint

--- frontend/src/shared/api/api.ts
+++ frontend/src/shared/api/api.ts
@@ -1,6 +1,10 @@
 import { BaseQueryApi, FetchArgs, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { setUserIsAuth } from '../store/reducers/UserSlice';
 
+import JSONbig from 'json-bigint';
+
+const JSONbigString = JSONbig({ storeAsString: true });
+
 const baseQuery = fetchBaseQuery({
   baseUrl: `${import.meta.env.VITE_S_URL}`,
   prepareHeaders: (headers) => {
@@ -11,6 +15,21 @@ const baseQuery = fetchBaseQuery({
     headers.set('Content-Type', 'application/json');
     headers.set('Accept', 'application/json');
     return headers;
+  },
+  fetchFn: async (url, config) => {
+    const response = await fetch(url, config);
+    if (!response.ok) {
+      // Если ответ не окей, возвращаем его как есть, чтобы RTK Query мог обработать ошибку
+      return response;
+    }
+    const text = await response.text();
+    const data = text.length ? JSONbigString.parse(text) : undefined;
+    // Создаем новый объект Response с преобразованными данными
+    return new Response(JSON.stringify(data), {
+      status: response.status,
+      statusText: response.statusText,
+      headers: response.headers,
+    });
   },
 });
 
